d1e7e51ebc6b475a14536c81f5fe95d31bac92db
Ezequiel Ribas Figueira junior <juniorfigueira0800@gmail.com>
2023-03-31 10:46:54 -0300

    TTS

---
+++ TTS/app/src/main/java/com/example/tts/TextToSpeechHelper.java
@@ -0,0 +1,33 @@
+package com.example.tts;
+
+import android.content.Context;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class TextToSpeechHelper implements TextToSpeech.OnInitListener {
+    private TextToSpeech tts;
+    private boolean isReady = false;
+
+    public TextToSpeechHelper(Context context) {
+        tts = new TextToSpeech(context, this);
+    }
+
+    public void speak(String text) {
+        if (isReady) {
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            tts.setLanguage(Locale.getDefault());
+            isReady = true;
+        }else {
+            Log.d("TTS","Falha na inicialização TTs");
+        }
+    }
+}
